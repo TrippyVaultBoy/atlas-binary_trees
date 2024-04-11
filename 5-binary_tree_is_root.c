146d886552e41d9e824eea81d1cadf101b996965
Tanner Saint <tsaint2004@gmail.com>
2024-04-11 20:49:57 +0000

    added code to 5-binary_tree_is_root.c

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root- determines if node is a root
+ * @node: node to be checked
+ *
+ * Return: 1 if root 0 if not or node is null
+*/
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0);
+	}
+
+	if (node->parent == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
