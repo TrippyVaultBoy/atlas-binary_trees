2ac3088532401604a676b3a731ee3601694a4aae
Tanner Saint <tsaint2004@gmail.com>
2024-04-11 19:32:14 +0000

    made betty compliant

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,25 +10,25 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-    if (new_node == NULL)
-    {
-        return (NULL);
-    }
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 
-    if (parent == NULL)
-    {
-        new_node->parent = NULL;
-    }
-    else
-    {
-        new_node->parent = parent;
-    }
+	if (parent == NULL)
+	{
+		new_node->parent = NULL;
+	}
+	else
+	{
+		new_node->parent = parent;
+	}
 
-    new_node->n = value;
-    new_node->left = NULL;
-    new_node->right = NULL;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-    return (new_node);
+	return (new_node);
 }
