bf1bc3c03eedbadf2954163211a9647c65a0647d
Tanner Saint <tsaint2004@gmail.com>
2024-04-11 20:22:33 +0000

    added documentation

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,18 +1,19 @@
 #include "binary_trees.h"
 
 /**
- *
+ * binary_tree_delete- deleted an entire binary tree
+ * @tree: pointer to the root of the tree
 */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return;
-    }
+	if (tree == NULL)
+	{
+		return;
+	}
 
-    binary_tree_delete(tree->left);
-    binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
 
-    free(tree);
+	free(tree);
 }
