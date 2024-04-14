02f36381445e9bb2991a05bf194254e77b23a0c2
Tanner Saint <tsaint2004@gmail.com>
2024-04-14 02:50:43 +0000

    added case of single node

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,6 +16,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
+	else if (tree->left == NULL && tree->right == NULL)
+    {
+        return (0);
+    }
 	else
 	{
 		left = 1 + binary_tree_height(tree->left);
