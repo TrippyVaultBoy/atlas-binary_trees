6552cdf0e1dd92bcb4c4b413e5b2f29b271e628a
Tanner Saint <tsaint2004@gmail.com>
2024-04-14 00:36:01 +0000

    added new line and documentation

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,7 +1,9 @@
 #include "binary_trees.h"
 
 /**
- *
+ * binary_tree_inorder- traverses a binary tree
+ * @tree: pointer to the root node of the tree
+ * @func: function to call for each node
 */
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
