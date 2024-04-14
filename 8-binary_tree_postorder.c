0e9a2d02ef939d30979e31e6a65dcedbf564c851
Tanner Saint <tsaint2004@gmail.com>
2024-04-14 00:44:14 +0000

    added new line and documentation

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,9 @@
 #include "binary_trees.h"
 
 /**
- *
+ * binary_tree_postorder- traverses a binary tree
+ * @tree: pointer to the root node of the tree
+ * @func: function to be called for each node
 */
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
