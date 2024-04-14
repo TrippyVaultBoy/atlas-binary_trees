f2f5726bd0988c9a63cbde718f7d236e394d3cd2
Tanner Saint <tsaint2004@gmail.com>
2024-04-14 01:11:03 +0000

    added code to 9-binary_tree_height.c

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+
+/**
+ *
+*/
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int left;
+	int right;
+
+	left = 0;
+	right = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		left = binary_tree_height(tree->left);
+		right = binary_tree_height(tree->right);
+
+		if (right > left)
+		{
+			return (right + 1);
+		}
+		else if (left > right)
+		{
+			return (left + 1);
+		}
+		else if (right == left)
+		{
+			return (left + 1);
+		}
+	}
+}
